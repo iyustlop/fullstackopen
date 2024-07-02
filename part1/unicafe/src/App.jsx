bb0c1989c7e161aaa765071913d1fbdfd4e741ca
Ignacio Yuste Lopez <iyustlop@gmail.com>
2024-07-03 00:12:54 +0200

    1.11 unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,34 +1,46 @@
 import { useState } from 'react'
 
-const StatisticLine = props => <div>{props.text} {props.value}</div>
+const StatisticLine = props => { return (<><td>{props.text}</td><td> {props.value}</td></>) }
 
-const Statistics = (props) => { 
-  if (props.good === 0 && props.neutral === 0 && props.bad === 0){
+const Statistics = (props) => {
+  if (props.good === 0 && props.neutral === 0 && props.bad === 0) {
     return (
       <p>No feedback given</p>
     )
-  } 
-  
+  }
+
   console.log(props)
 
   const all = props.good + props.neutral + props.bad
   console.log(all)
 
-  const average = ((props.good - props.bad)/all)
+  const average = ((props.good - props.bad) / all)
   console.log(average || 0)
 
-  const positive = ((props.good/all)*100 || 0).toString().concat(' %')
+  const positive = ((props.good / all) * 100 || 0).toString().concat(' %')
   console.log(positive || 0)
 
   return (
-    <div>
-      <StatisticLine text='good' value={props.good}></StatisticLine>
-      <StatisticLine text='neutral' value={props.neutral}></StatisticLine>
-      <StatisticLine text='bad' value={props.bad}></StatisticLine>
-      <StatisticLine text='all' value={all}/>
-      <StatisticLine text='average' value={average || 0}/>
-      <StatisticLine text='positive' value={positive || 0}/>
-    </div>
+    <>
+      <tr>
+        <StatisticLine text='good' value={props.good}></StatisticLine>
+      </tr>
+      <tr>
+        <StatisticLine text='neutral' value={props.neutral}></StatisticLine>
+      </tr>
+      <tr>
+        <StatisticLine text='bad' value={props.bad}></StatisticLine>
+      </tr>
+      <tr>
+        <StatisticLine text='all' value={all} />
+      </tr>
+      <tr>
+        <StatisticLine text='average' value={average || 0} />
+      </tr>
+      <tr>
+        <StatisticLine text='positive' value={positive || 0} />
+      </tr>
+    </>
   )
 }
 
@@ -48,13 +60,13 @@ const App = () => {
     <>
       <div>
         <h1>give feedback</h1>
-          <Button handleClick={() => setGood(good + 1)} text="good" />
-          <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
-          <Button handleClick={() => setBad(bad + 1)} text="bad" />
+        <Button handleClick={() => setGood(good + 1)} text="good" />
+        <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+        <Button handleClick={() => setBad(bad + 1)} text="bad" />
       </div>
       <div>
         <h1>statistics</h1>
-        <Statistics good={good} neutral={neutral} bad={bad}/>
+        <Statistics good={good} neutral={neutral} bad={bad} />
       </div>
     </>
   )
