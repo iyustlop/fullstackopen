9a1f654dcfa279e2c3b4c0c301e917f17c6b5569
iyustlop <iyustlop@gmail.com>
2024-07-05 20:36:13 +0200

    2.7 phonebook string templates

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -7,17 +7,27 @@ const App = () => {
   const [newName, setNewName] = useState('')
 
   const handleNameChange = (event) => {
-    console.log(event.target.value)
     setNewName(event.target.value)
   }
+
   const addName = (event) => {
     event.preventDefault()
     const noteObject = {
       name: newName,
     }
-  
-    setPersons(persons.concat(noteObject))
-    setNewName('')
+    
+    
+    console.log('añado el nombre', noteObject)
+    console.log('lista', persons)
+    const index = persons.findIndex(i => i.name === noteObject.name);
+    console.log('indice', index)
+    if(index === -1) {
+      setPersons(persons.concat(noteObject))
+      setNewName('')
+    }
+    else{
+      alert(`${noteObject.name} is already added to phonebook`);
+    }
   }
 
   return (
