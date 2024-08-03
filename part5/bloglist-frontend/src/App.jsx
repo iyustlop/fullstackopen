0522e23d471f67a953d9c609a02f6dd64b5c3789
iyustlop <iyustlop@gmail.com>
2024-08-03 12:17:27 +0200

    5.3 blogs created in mongoDB

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -7,19 +7,22 @@ const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
   const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+      setBlogs(blogs)
+    )
   }, [])
 
   useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem('loggedBlogsappUser')
     if (loggedUserJSON) {
       console.log(loggedUserJSON)
-      
+
       const user = JSON.parse(loggedUserJSON)
       setUser(user)
       blogService.setToken(user.token)
@@ -34,8 +37,8 @@ const App = () => {
       })
       window.localStorage.setItem(
         'loggedBlogsappUser', JSON.stringify(user)
-      ) 
-      
+      )
+
       setUser(user)
       setUsername('')
       setPassword('')
@@ -47,41 +50,65 @@ const App = () => {
     }
   }
 
-const handleCloseSession = async (event) => {
-  window.localStorage.removeItem('loggedBlogsappUser')
-  setUser(null)
-  setUsername('')
-  setPassword('')
-}
+  const handleCloseSession = async (event) => {
+    window.localStorage.removeItem('loggedBlogsappUser')
+    setUser(null)
+    setUsername('')
+    setPassword('')
+  }
 
-const loginForm = () => {
-  return(
-    <form onSubmit={handleLogin}>
-    <div>
-      username:
-      <input
-        type='text'
-        value={ username }
-        name='username'
-        onChange={ ({ target }) => setUsername(target.value)}
-      />
-      password:
-      <input
-        type='password'
-        value={password}
-        name='password'
-        onChange={ ({ target }) => setPassword(target.value)}
-      />
-      <button type='submit'>login</button>
-    </div>
-  </form>
-  )
-}
+  const handleCreateNewBlog = async () => {
+    const blog = {
+      title: title,
+      author: author,
+      url: url
+    }
+    console.log(blog)
+    
+    blogService.saveOneBlog(blog)
+  }
+
+  const loginForm = () => {
+    return (
+      <form onSubmit={handleLogin}>
+        <div>
+          username:
+          <input
+            type='text'
+            value={username}
+            name='username'
+            onChange={({ target }) => setUsername(target.value)}
+          />
+          password:
+          <input
+            type='password'
+            value={password}
+            name='password'
+            onChange={({ target }) => setPassword(target.value)}
+          />
+          <button type='submit'>login</button>
+        </div>
+      </form>
+    )
+  }
+
+  const createBlog = () => {
+    return (
+      <>
+        <h2>Create new</h2>
+        <div>title <input onChange={({ target }) => setTitle(target.value)}/></div>
+        <div>author <input onChange={({ target }) => setAuthor(target.value)}/></div>
+        <div>url <input onChange={({target}) => setUrl(target.value)}/></div>
+        <button onClick={handleCreateNewBlog}>create</button>
+      </>
+    )
+  }
 
   return (
     <div>
-      { user === null ? loginForm() : <div><p>{user.name} logged-in</p><button onClick={handleCloseSession}>logout</button></div>}
-      { user !== null && <h2>blogs</h2> }
+      {user === null ? loginForm() : <div><p>{user.name} logged-in</p><button onClick={handleCloseSession}>logout</button></div>}
+      {user !== null && createBlog()}
+      {user !== null && <h2>blogs</h2>}
       {user !== null && blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -7,9 +7,22 @@ const setToken = newToken => {
   token = `Bearer ${newToken}`
 }
 
+const getToken = () => {
+  return token
+}
+
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
 
-export default { getAll, setToken }
+const saveOneBlog = async (blog) => {
+  const config = {
+    headers: { Authorization: token },
+  }
+
+  const response = axios.post(baseUrl, blog, config)
+  return response.data
+}
+
+export default { getAll, saveOneBlog, setToken, getToken }
