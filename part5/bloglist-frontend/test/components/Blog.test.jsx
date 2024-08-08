493233b992adfaef252b46acd2b110c356579cee
iyustlop <iyustlop@gmail.com>
2024-08-08 15:52:32 +0200

    5.15 twice click button Like

--- part5/bloglist-frontend/test/components/Blog.test.jsx
+++ part5/bloglist-frontend/test/components/Blog.test.jsx
@@ -19,7 +19,7 @@ test('renders content', () => {
   expect(div).toHaveTextContent(blog.author)
 })
 
-test('clicking the buttonLike calls event handler once', async () => {
+test('clicking the view button url and likes are displayed', async () => {
   const mockHandler = vi.fn()
 
   const { container } = render(<Blog blog={blog} handleLike={mockHandler}/>)
@@ -32,11 +32,26 @@ test('clicking the buttonLike calls event handler once', async () => {
   expect(div).not.toHaveTextContent(blog.likes)
 
   const user = userEvent.setup()
-  const buttonLike = screen.getByText('view')
-  await user.click(buttonLike)
+  const buttonView = screen.getByText('view')
+  await user.click(buttonView)
 
   expect(div).toHaveTextContent(blog.url)
   expect(div).toHaveTextContent(blog.likes)
+})
+
+test('clicking the buttonLike calls event handler once', async () => {
+  const mockHandler = vi.fn()
+
+  render(<Blog blog={blog} handleLike={mockHandler}/>)
+
+  const user = userEvent.setup()
+  const buttonView = screen.getByText('view')
+  await user.click(buttonView)
+  const buttonLike = screen.getByText('like')
+  await user.click(buttonLike)
+  await user.click(buttonLike)
+
+  expect(mockHandler.mock.calls).toHaveLength(2)
 })
 
 test('clicking the buttonLike like ', async () => {
