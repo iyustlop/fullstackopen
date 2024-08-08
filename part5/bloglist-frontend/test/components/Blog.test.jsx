72758ce97f40113d30b93ccfb19f85721d88c834
iyustlop <iyustlop@gmail.com>
2024-08-08 16:12:26 +0200

    5.16 createBlogForm Test

--- part5/bloglist-frontend/test/components/Blog.test.jsx
+++ part5/bloglist-frontend/test/components/Blog.test.jsx
@@ -10,10 +10,17 @@ const blog = {
   user:  { username: 'iyustlop' }
 }
 
+const buttons = {
+  view: 'view',
+  like: 'like',
+}
+
+const cssSelectorBlog = '.blog'
+
 test('renders content', () => {
   const { container } = render(<Blog blog={blog} />)
 
-  const div = container.querySelector('.blog')
+  const div = container.querySelector(cssSelectorBlog)
 
   expect(div).toHaveTextContent(blog.title)
   expect(div).toHaveTextContent(blog.author)
@@ -24,7 +31,7 @@ test('clicking the view button url and likes are displayed', async () => {
 
   const { container } = render(<Blog blog={blog} handleLike={mockHandler}/>)
 
-  const div = container.querySelector('.blog')
+  const div = container.querySelector(cssSelectorBlog)
 
   expect(div).toHaveTextContent(blog.title)
   expect(div).toHaveTextContent(blog.author)
@@ -32,43 +39,24 @@ test('clicking the view button url and likes are displayed', async () => {
   expect(div).not.toHaveTextContent(blog.likes)
 
   const user = userEvent.setup()
-  const buttonView = screen.getByText('view')
+  const buttonView = screen.getByText(buttons.view)
   await user.click(buttonView)
 
   expect(div).toHaveTextContent(blog.url)
   expect(div).toHaveTextContent(blog.likes)
 })
 
-test('clicking the buttonLike calls event handler once', async () => {
+test('clicking the buttonLike two times calls event handler twice', async () => {
   const mockHandler = vi.fn()
 
   render(<Blog blog={blog} handleLike={mockHandler}/>)
 
   const user = userEvent.setup()
-  const buttonView = screen.getByText('view')
+  const buttonView = screen.getByText(buttons.view)
   await user.click(buttonView)
-  const buttonLike = screen.getByText('like')
+  const buttonLike = screen.getByText(buttons.like)
   await user.click(buttonLike)
   await user.click(buttonLike)
 
   expect(mockHandler.mock.calls).toHaveLength(2)
-})
-
-test('clicking the buttonLike like ', async () => {
-  const mockHandler = vi.fn()
-
-  const { container } = render(<Blog blog={blog} handleLike={mockHandler}/>)
-
-  const div = container.querySelector('.blog')
-
-  expect(div).toHaveTextContent(blog.author)
-
-  const user = userEvent.setup()
-  const buttonView = screen.getByText('view')
-  await user.click(buttonView)
-  const buttonLike = screen.getByText('like')
-  await user.click(buttonLike)
-
-  expect(mockHandler.mock.calls).toHaveLength(1)
-
 })
--- part5/bloglist-frontend/test/components/CreateBlogForm.test.jsx
+++ part5/bloglist-frontend/test/components/CreateBlogForm.test.jsx
@@ -2,22 +2,36 @@ import { render, screen } from '@testing-library/react'
 import CreateBlogForm from '../../src/components/CreateBlogForm'
 import userEvent from '@testing-library/user-event'
 
+const blog = {
+  title: 'testing title',
+  author: 'testing author',
+  url: 'testing url'
+}
+
+const placeHolderText = {
+  title: 'Blog title',
+  author: 'Blog author',
+  url: 'blog url'
+}
+
 test('<NoteForm /> updates parent state and calls onSubmit', async () => {
   const createBlog = vi.fn()
   const user = userEvent.setup()
 
   render(<CreateBlogForm createBlog={createBlog} />)
 
-  const inputTtile = screen.getByPlaceholderText('Blog title')
-  const inputAuthor = screen.getByPlaceholderText('Blog author')
-  const inputTUrl = screen.getByPlaceholderText('blog url')
+  const inputTtile = screen.getByPlaceholderText(placeHolderText.title)
+  const inputAuthor = screen.getByPlaceholderText(placeHolderText.author)
+  const inputTUrl = screen.getByPlaceholderText(placeHolderText.url)
   const sendButton = screen.getByText('create')
 
-  await user.type(inputTtile, 'testing a form...')
-  await user.type(inputAuthor, 'testing a form...')
-  await user.type(inputTUrl, 'testing a form...')
+  await user.type(inputTtile, blog.title)
+  await user.type(inputAuthor, blog.author)
+  await user.type(inputTUrl, blog.url)
   await user.click(sendButton)
 
   expect(createBlog.mock.calls).toHaveLength(1)
-  //expect(createBlog.mock.calls[0][0].content).toBe('testing a form...')
+  expect(createBlog.mock.calls[0][0].title).toBe(blog.title)
+  expect(createBlog.mock.calls[0][0].author).toBe(blog.author)
+  expect(createBlog.mock.calls[0][0].url).toBe(blog.url)
 })
