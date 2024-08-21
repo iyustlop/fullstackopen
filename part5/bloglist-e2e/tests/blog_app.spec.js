ac4db572fc45c928b6c07366d0421fd92d1c77f4
iyustlop <iyustlop@gmail.com>
2024-08-21 14:08:08 +0200

    5.19 Test Passed

--- part5/bloglist-e2e/tests/blog_app.spec.js
+++ part5/bloglist-e2e/tests/blog_app.spec.js
@@ -1,5 +1,9 @@
 const { test, describe, expect, beforeEach } = require('@playwright/test')
 
+const USER_LOGED_IN = 'Matti Luukkainen logged in'
+const USERNAME = 'mluukkai'
+const PASSWORD = 'salainen'
+
 describe('Blog list App', () => {
 
   beforeEach(async ({ page, request }) => {
@@ -25,38 +29,45 @@ describe('Blog list App', () => {
     test('succeeds with correct credentials', async ({ page }) => {
       await page.getByRole('button', { name: 'log in' }).click()
 
-      await page.getByTestId('username').fill('mluukkai')
-      await page.getByTestId('password').fill('salainen')
+      await page.getByTestId('username').fill(USERNAME)
+      await page.getByTestId('password').fill(PASSWORD)
       await page.getByRole('button', { name: 'login' }).click()
-      await expect(page.getByText('mluukkai logged-in')).toBeVisible()
+      await expect(page.getByText(USER_LOGED_IN)).toBeVisible()
     })
 
     test('login fails with wrong password', async ({ page }) => {
       await page.getByRole('button', { name: 'log in' }).click()
 
-      await page.locator('.username').fill('mluukkai')
-      await page.locator('.password').fill('wrong')
+      await page.getByTestId('username').fill(USERNAME)
+      await page.getByTestId('password').fill('wrong')
       await page.getByRole('button', { name: 'login' }).click()
 
       const errorDiv = await page.locator('.error')
       await expect(errorDiv).toContainText('Wrong user and password')
+      await expect(errorDiv).toHaveCSS('border-style', 'solid')
+      await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)')
+      await expect(page.getByText(USER_LOGED_IN)).not.toBeVisible()
+
     })
   })
 
   describe('when logged in', () => {
     beforeEach(async ({ page }) => {
+      await new Promise(r => setTimeout(r, 5000));
       await page.getByRole('button', { name: 'log in' }).click()
-      await page.getByTestId('username').fill('mluukkai')
-      await page.getByTestId('password').fill('salainen')
+      await page.getByTestId('username').fill(USERNAME)
+      await page.getByTestId('password').fill(PASSWORD)
       await page.getByRole('button', { name: 'login' }).click()
-      await expect(page.getByText('mluukkai logged-in')).toBeVisible()
+      await expect(page.getByText(USER_LOGED_IN)).toBeVisible()
     })
 
     test('a new blog can be created', async ({ page }) => {
       await page.getByRole('button', { name: 'Create new blog' }).click()
-      await page.getByRole('textbox').fill('a note created by playwright')
-      await page.getByRole('button', { name: 'save' }).click()
-      await expect(page.getByText('a note created by playwright')).toBeVisible()
+      await page.getByTestId('title').fill('a blog created by playwright')
+      await page.getByTestId('author').fill('a blog author created by playwright')
+      await page.getByTestId('url').fill('a blog url created by playwright')
+      await page.getByRole('button', { name: 'create' }).click()
+      await expect(page.getByText('A new blog a blog created by playwright')).toBeVisible()
     })
   })  
 })
