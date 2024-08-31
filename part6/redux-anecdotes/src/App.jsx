9d6e1ce55517ecdd08b60b681cf15d5ee1f9647d
iyustlop <Log1t3ch>
2024-08-31 16:32:35 +0200

    6.14 receive data from backend

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,9 +1,20 @@
+import { useEffect } from 'react'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
+import { useDispatch } from 'react-redux'
+import anecdotesService from './services/anecdotesService'
+import { setAnecdotes } from './reducers/anecdoteReducer'
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    anecdotesService
+      .getAll()
+      .then(notes => dispatch(setAnecdotes(notes)))
+  })
+
   return (
     <div>
       <Notification />
