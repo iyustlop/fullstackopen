8e93a4824b13f38a2f42ca81f26824a3dff6fbcd
iyustlop <Log1t3ch>
2024-09-09 23:34:06 +0200

    7.7 Country flags

---
+++ part7/country-hook/src/services/countrySerivice.js
@@ -0,0 +1,11 @@
+import axios from "axios"
+
+    const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api/name'
+
+    const getCountry = async (name) => {
+        const response = await axios.get(`${baseUrl}/${name}`)
+        
+        return response
+    }
+
+export default { getCountry}
